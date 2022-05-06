27d50a1a46c5566aefb593065c62d11551b26270
chuangcaleb <balechuang@gmail.com>
2022-05-06 08:46:30 +0100

    arch: add Condition and Effect classes

--- intchef/goap/actions.py
+++ intchef/goap/actions.py
@@ -2,13 +2,14 @@
 Defines all GOAP actions
 """
 
+from typing import Dict
 from .colors import red
 from .components import ComponentList
 
 
 class Action:
 
-    def __init__(self, name: str, precond, effect):
+    def __init__(self, name: str, precond: 'Condition', effect: 'Effect'):
         self.name = name
         self.precond = precond
         self.effect = effect
@@ -17,29 +18,41 @@ class Action:
         return red(self.name)
 
 
+class Condition(Dict):
+
+    def __init__(self, initial_state: Dict[ComponentList, int], *args, **kw):
+        super(Condition, self).__init__(initial_state, *args, **kw)
+
+
+class Effect(Dict):
+
+    def __init__(self, effects: Dict[int, Condition], *args, **kw):
+        super(Effect, self).__init__(effects, *args, **kw)
+
+
 class ActionList:
 
     IDLE = Action(
         name="Do Nothing",
-        precond={},
-        effect={}
+        precond=Condition({}),
+        effect=Effect({})
     )
     TOAST_BREAD = Action(
         name="Toast Bread",
-        precond={ComponentList.BREAD: 1},
-        effect={3: {ComponentList.TOAST: 1}}
+        precond=Condition({ComponentList.BREAD: 1}),
+        effect=Effect({3: {ComponentList.TOAST: 1}})
     )
 
     ASSEMBLE_SIMPLE_TOAST_SANDWICH = Action(
         name="Assemble Simple Toast Sandwich",
-        precond={
+        precond=Condition({
             ComponentList.TOAST: 2,
             ComponentList.HAM: 1,
             ComponentList.CHEESE: 1
-        },
-        effect={1: {ComponentList.SIMPLE_TOAST_SANDWICH: 1}}
+        }),
+        effect=Effect({1: {ComponentList.SIMPLE_TOAST_SANDWICH: 1}})
     )
 
 
-ALL_ACTIONS = [v for k, v in ActionList.__dict__.items()
+ALL_ACTIONS = [v for v in ActionList.__dict__.values()
                if isinstance(v, Action)]
