fb3995bde923f2b1ad22043924b527ddd9028658
chuangcaleb <balechuang@gmail.com>
2022-05-07 14:13:03 +0100

    reintr: Uncomment and bring back chef

--- intchef/goap/actions.py
+++ intchef/goap/actions.py
@@ -6,7 +6,7 @@ from typing import Dict
 
 from colours import Colour, colour
 from intchef.goap.abstract import KitchenResource
-from intchef.goap.components import ComponentList, EquipmentList, IngredientList
+from intchef.goap.components import ChefList, ComponentList, EquipmentList, IngredientList
 
 
 class Action:
@@ -44,13 +44,13 @@ class ActionList:
         name="Toast Bread",
 
         precond=Condition({
-            # ChefList.CHEF: 1,
+            ChefList.CHEF: 1,
             EquipmentList.TOASTER_SLOT: 1,
             IngredientList.BREAD: 1
         }),
 
         effect=Effect({
-            # 1: {ChefList.CHEF: 1},
+            1: {ChefList.CHEF: 1},
             3: {ComponentList.TOAST: 1,
                 EquipmentList.TOASTER_SLOT: 1}}
         )
@@ -59,35 +59,40 @@ class ActionList:
     ASSEMBLE_SIMPLE_TOAST_SANDWICH = Action(
         name="Assemble Simple Toast Sandwich",
         precond=Condition({
-            # ChefList.CHEF: 1,
+            ChefList.CHEF: 1,
             IngredientList.HAM: 1,
             IngredientList.CHEESE: 1,
             ComponentList.TOAST: 2
         }),
         effect=Effect({1: {
-            # ChefList.CHEF: 1,
+            ChefList.CHEF: 1,
             ComponentList.SIMPLE_TOAST_SANDWICH: 1}})
     )
 
     WASH_RICE = Action(
         name="Wash Rice",
         precond=Condition({
+            ChefList.CHEF: 1,
             IngredientList.RAW_RICE: 1,
             EquipmentList.POT: 1
         }),
-        effect=Effect({
-            2: {ComponentList.WASHED_RICE: 1,
-                EquipmentList.POT: 1}
+        effect=Effect({2: {
+            ChefList.CHEF: 1,
+            ComponentList.WASHED_RICE: 1,
+            EquipmentList.POT: 1
+        }
         })
     )
 
     COOK_RICE = Action(
         name="Cook Rice",
         precond=Condition({
+            ChefList.CHEF: 1,
             ComponentList.WASHED_RICE: 1,
             EquipmentList.RICE_COOKER: 1
         }),
         effect=Effect({
+            1: {ChefList.CHEF: 1},
             10: {ComponentList.RICE: 1, EquipmentList.RICE_COOKER: 1}
         })
     )
@@ -95,12 +100,12 @@ class ActionList:
     FRY_EGG = Action(
         name="Fry Egg",
         precond=Condition({
-            # ChefList.CHEF: 1,
+            ChefList.CHEF: 1,
             EquipmentList.PAN: 1,
             IngredientList.EGG: 1
         }),
         effect=Effect({2: {
-            # ChefList.CHEF: 1,
+            ChefList.CHEF: 1,
             EquipmentList.PAN: 1,
             ComponentList.FRIED_EGG: 1
         }})
@@ -109,13 +114,13 @@ class ActionList:
     DEBONE_THIGH = Action(
         name="Debone Thigh",
         precond=Condition({
-            # ChefList.CHEF: 1,
+            ChefList.CHEF: 1,
             IngredientList.RAW_CHICKEN_THIGH: 1,
             EquipmentList.KNIFE: 1,
             EquipmentList.BOARD: 1
         }),
         effect=Effect({1: {
-            # ChefList.CHEF: 1,
+            ChefList.CHEF: 1,
             ComponentList.RAW_CHICKEN_FILLET: 1,
             EquipmentList.KNIFE: 1,
             EquipmentList.BOARD: 1
@@ -125,12 +130,12 @@ class ActionList:
     FRY_FILLET = Action(
         name="Fry Chicken Fillet",
         precond=Condition({
-            # ChefList.CHEF: 1,
+            ChefList.CHEF: 1,
             ComponentList.RAW_CHICKEN_FILLET: 1,
             EquipmentList.PAN: 1
         }),
         effect=Effect({3: {
-            # ChefList.CHEF: 1,
+            ChefList.CHEF: 1,
             ComponentList.FRIED_CHICKEN_FILLET: 1,
             EquipmentList.PAN: 1
         }})
--- intchef/recipe_book.py
+++ intchef/recipe_book.py
@@ -10,7 +10,7 @@ class RecipeBook:
             ComponentList.TOAST: 1
         }),
         initial_state=WorldStateFrame({
-            # ChefList.CHEF: 1,
+            ChefList.CHEF: 1,
             EquipmentList.TOASTER_SLOT: 1,
             IngredientList.BREAD: 1
         })
@@ -21,7 +21,7 @@ class RecipeBook:
             ComponentList.SIMPLE_TOAST_SANDWICH: 1
         }),
         initial_state=WorldStateFrame({
-            # ChefList.CHEF: 1,
+            ChefList.CHEF: 1,
             EquipmentList.TOASTER_SLOT: 2,
             IngredientList.BREAD: 2,
             IngredientList.HAM: 1,
@@ -34,7 +34,7 @@ class RecipeBook:
             ComponentList.FRIED_EGG: 1
         }),
         initial_state=WorldStateFrame({
-            # ChefList.CHEF: 1,
+            ChefList.CHEF: 1,
             EquipmentList.PAN: 1,
             IngredientList.EGG: 1,
         })
@@ -45,7 +45,7 @@ class RecipeBook:
             ComponentList.FRIED_CHICKEN_FILLET: 1
         }),
         initial_state=WorldStateFrame({
-            # ChefList.CHEF: 1,
+            ChefList.CHEF: 1,
             EquipmentList.PAN: 1,
             EquipmentList.KNIFE: 1,
             EquipmentList.BOARD: 1,
@@ -60,7 +60,7 @@ class RecipeBook:
             ComponentList.RICE: 1
         }),
         initial_state=WorldStateFrame({
-            # ChefList.CHEF: 1,
+            ChefList.CHEF: 1,
             EquipmentList.PAN: 1,
             EquipmentList.KNIFE: 1,
             EquipmentList.RICE_COOKER: 1,
